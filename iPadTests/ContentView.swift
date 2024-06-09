b82da58dce14b392839ca0432d2e5c911679a6f0
Henny2 <henrieke.baunack@web.de>
2024-06-09 09:29:24 -1000

    navigation split view for ipad

--- iPadTests/ContentView.swift
+++ iPadTests/ContentView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationSplitView(columnVisibility: .constant(.all), preferredCompactColumn: .constant(.detail)){
+            NavigationLink("Primary"){
+                Color.green
+            }
+        } detail: {
+            Text("Content")
+        }.navigationSplitViewStyle(.balanced)
     }
 }
 
