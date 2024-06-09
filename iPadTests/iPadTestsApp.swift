d702b755beeefb234b2dc8e4cfce4e57faf75278
Henny2 <henrieke.baunack@web.de>
2024-06-09 09:09:18 -1000

    Initial Commit

---
+++ iPadTests/iPadTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPadTestsApp.swift
+//  iPadTests
+//
+//  Created by Henrieke Baunack on 6/9/24.
+//
+
+import SwiftUI
+
+@main
+struct iPadTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
